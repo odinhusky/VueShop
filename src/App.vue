d68a18a61698c7c1fb29a5a5467d6244d14a5569
Odin Husky <odinhusky0923@gmail.com>
2020-08-04 16:59:16 +0800

    20200804-2 update

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,20 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  // hook
+  created() {
+    // const api = 'https://vue-course-api.hexschool.io/api/odinhusky/products';
+    const api = `${process.env.VUE_APP_APIDOMAIN}/api/${process.env.VUE_APPAPI_PATH}/products`;
+    this.$http.get(api).then((res) => {
+      console.log('res', res);
+    });
+  },
+};
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
