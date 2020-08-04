5341350f7641bac4a745ee2f2d5cc7f9acca5f21
Odin Husky <odinhusky0923@gmail.com>
2020-08-04 18:10:20 +0800

    20200804-4 update

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <a href="#" @click.prevent="signout">登出</a>
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    signout() {
+      const vm = this;
+      const api = `${process.env.VUE_APP_APIDOMAIN}/logout`;
+      this.$http.post(api)
+        .then((res) => {
+          console.log('logout => res', res);
+          if (res.data.success) {
+            vm.$router.push('/Login');
+          }
+        });
+    },
+  },
+};
+</script>
---
+++ src/components/pages/Login.vue
@@ -0,0 +1,116 @@
+<template>
+    <div>
+        <form class="form-signin" @submit.prevent="signin">
+            <img
+                class="mb-4"
+                src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt=""
+                width="72" height="72"
+            >
+            <h1 class="h3 mb-3 font-weight-normal">請登入</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input
+                type="email"
+                id="inputEmail"
+                class="form-control"
+                placeholder="Email address"
+                required
+                autofocus
+                v-model="user.username"
+            >
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input
+                type="password"
+                id="inputPassword"
+                class="form-control"
+                placeholder="Password"
+                required
+                v-model="user.password"
+            >
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me">
+                    記住我
+                </label>
+            </div>
+            <button class="btn btn-lg btn-odin btn-block" type="submit">登入</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+
+  methods: {
+    signin() {
+      const vm = this;
+      const api = `${process.env.VUE_APP_APIDOMAIN}/signin`;
+      this.$http.post(api, vm.user)
+        .then((res) => {
+          console.log('res', res);
+          if (res.data.success) {
+            vm.$router.push('/');
+          }
+        });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+    html,
+    body {
+    height: 100%;
+    }
+
+    body {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+    }
+
+    .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+    }
+    .form-signin .checkbox {
+    font-weight: 400;
+    }
+    .form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+    }
+    .form-signin .form-control:focus {
+    z-index: 2;
+    }
+    .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+    }
+    .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    }
+</style>
